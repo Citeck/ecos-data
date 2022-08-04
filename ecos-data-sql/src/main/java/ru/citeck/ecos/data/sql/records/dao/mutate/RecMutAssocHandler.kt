a8f031e6a7ac994c258e38ed0be4b614eea14da2
Pavel Simonov <pavel.simonov@citeck.ru>
2022-08-04 09:41:32 +0700

    ECOSENT-2334 - Child record can't become associated with parent via action "create"

--- ecos-data-sql/src/main/java/ru/citeck/ecos/data/sql/records/dao/mutate/RecMutAssocHandler.kt
+++ ecos-data-sql/src/main/java/ru/citeck/ecos/data/sql/records/dao/mutate/RecMutAssocHandler.kt
@@ -19,6 +19,9 @@ class RecMutAssocHandler(private val ctx: DbRecordsDaoCtx) {
 
     companion object {
         private val log = KotlinLogging.logger {}
+
+        private const val MUTATION_FROM_PARENT_FLAG = "__mutationFromParent"
+        private const val MUTATION_FROM_CHILD_FLAG = "__mutationFromChild"
     }
 
     fun preProcessContentAtts(recAttributes: ObjectData, recToMutate: DbEntity, columns: List<EcosAttColumnDef>) {
@@ -105,16 +108,23 @@ class RecMutAssocHandler(private val ctx: DbRecordsDaoCtx) {
 
     fun replaceRefsById(recAttributes: ObjectData, columns: List<EcosAttColumnDef>) {
 
-        val assocAttsDef = columns.filter { DbRecordsUtils.isAssocLikeAttribute(it.attribute) }
+        val assocAtts = columns.filter {
+            DbRecordsUtils.isAssocLikeAttribute(it.attribute)
+        }.map { it.attribute.id }
+            .toMutableList()
 
-        if (assocAttsDef.isNotEmpty()) {
+        if (recAttributes.has(RecordConstants.ATT_PARENT)) {
+            assocAtts.add(RecordConstants.ATT_PARENT)
+        }
+
+        if (assocAtts.isNotEmpty()) {
             val assocRefs = mutableSetOf<RecordRef>()
-            assocAttsDef.forEach {
-                if (recAttributes.has(it.attribute.id)) {
-                    extractRecordRefs(recAttributes.get(it.attribute.id), assocRefs)
+            for (assocId in assocAtts) {
+                if (recAttributes.has(assocId)) {
+                    extractRecordRefs(recAttributes[assocId], assocRefs)
                 } else {
                     OperationType.values().forEach { op ->
-                        val attValue = recAttributes.get(op.prefix + it.attribute.id)
+                        val attValue = recAttributes[op.prefix + assocId]
                         extractRecordRefs(attValue, assocRefs)
                     }
                 }
@@ -127,21 +137,15 @@ class RecMutAssocHandler(private val ctx: DbRecordsDaoCtx) {
                     idByRef[ref] = refsId[idx]
                 }
             }
-            assocAttsDef.forEach {
-                if (recAttributes.has(it.attribute.id)) {
-                    recAttributes.set(
-                        it.attribute.id,
-                        replaceRecordRefsToId(recAttributes.get(it.attribute.id), idByRef)
-                    )
+            assocAtts.forEach { assocId ->
+                if (recAttributes.has(assocId)) {
+                    recAttributes[assocId] = replaceRecordRefsToId(recAttributes[assocId], idByRef)
                 } else {
                     OperationType.values().forEach { op ->
-                        val attWithPrefix = op.prefix + it.attribute.id
+                        val attWithPrefix = op.prefix + assocId
                         if (recAttributes.has(attWithPrefix)) {
-                            val value = recAttributes.get(attWithPrefix)
-                            recAttributes.set(
-                                attWithPrefix,
-                                replaceRecordRefsToId(value, idByRef)
-                            )
+                            val value = recAttributes[attWithPrefix]
+                            recAttributes[attWithPrefix] = replaceRecordRefsToId(value, idByRef)
                         }
                     }
                 }
@@ -182,9 +186,80 @@ class RecMutAssocHandler(private val ctx: DbRecordsDaoCtx) {
         return DataValue.NULL
     }
 
-    fun processChildrenAfterMutation(recBeforeSave: DbEntity, recAfterSave: DbEntity, columns: List<EcosAttColumnDef>) {
+    fun processParentAfterMutation(
+        recBeforeSave: DbEntity,
+        recAfterSave: DbEntity,
+        attributes: ObjectData
+    ) {
+        if (!attributes.has(RecordConstants.ATT_PARENT) || attributes.get(MUTATION_FROM_PARENT_FLAG, false)) {
+            return
+        }
+        val currentRef = ctx.recordRefService.getRecordRefById(recAfterSave.refId)
+        if (RecordRef.isEmpty(currentRef)) {
+            return
+        }
 
-        if (recAfterSave.refId < 0) {
+        val parentAttBefore = recBeforeSave.attributes[RecordConstants.ATT_PARENT_ATT] as? String ?: ""
+        val parentRefIdBefore = recBeforeSave.attributes[RecordConstants.ATT_PARENT] as? Long
+        val parentAttAfter = recAfterSave.attributes[RecordConstants.ATT_PARENT_ATT] as? String ?: ""
+        val parentRefIdAfter = recAfterSave.attributes[RecordConstants.ATT_PARENT] as? Long
+
+        if (parentRefIdBefore == parentRefIdAfter && parentAttBefore == parentAttAfter) {
+            return
+        }
+        val parentRefBefore = parentRefIdBefore?.let {
+            ctx.recordRefService.getRecordRefById(it)
+        } ?: RecordRef.EMPTY
+
+        if (RecordRef.isNotEmpty(parentRefBefore)) {
+            if (parentRefIdBefore == parentRefIdAfter) {
+                // changed only parent attribute
+                val atts = ObjectData.create()
+                if (parentAttBefore.isNotEmpty()) {
+                    atts[OperationType.ATT_REMOVE.prefix + parentAttBefore] = currentRef
+                }
+                if (parentAttAfter.isNotEmpty()) {
+                    atts[OperationType.ATT_ADD.prefix + parentAttAfter] = currentRef
+                }
+                if (atts.isNotEmpty()) {
+                    atts[MUTATION_FROM_CHILD_FLAG] = true
+                    ctx.recordsService.mutate(RecordAtts(parentRefBefore, atts))
+                }
+            } else {
+                // child was moved from one parent to another
+                val atts = ObjectData.create()
+                if (parentAttBefore.isNotEmpty()) {
+                    atts[OperationType.ATT_REMOVE.prefix + parentAttBefore] = currentRef
+                }
+                if (atts.isNotEmpty()) {
+                    atts[MUTATION_FROM_CHILD_FLAG] = true
+                    ctx.recordsService.mutate(RecordAtts(parentRefBefore, atts))
+                }
+            }
+        }
+
+        val parentRefAfter = parentRefIdAfter?.let {
+            ctx.recordRefService.getRecordRefById(it)
+        } ?: RecordRef.EMPTY
+
+        if (parentRefIdBefore == parentRefIdAfter || RecordRef.isEmpty(parentRefAfter) || parentAttAfter.isEmpty()) {
+            return
+        }
+
+        val atts = ObjectData.create()
+        atts[OperationType.ATT_ADD.prefix + parentAttAfter] = currentRef
+        atts[MUTATION_FROM_CHILD_FLAG] = true
+        ctx.recordsService.mutate(RecordAtts(parentRefAfter, atts))
+    }
+
+    fun processChildrenAfterMutation(
+        recBeforeSave: DbEntity,
+        recAfterSave: DbEntity,
+        attributes: ObjectData,
+        columns: List<EcosAttColumnDef>
+    ) {
+
+        if (recAfterSave.refId < 0 || attributes.get(MUTATION_FROM_CHILD_FLAG, false)) {
             return
         }
 
@@ -192,19 +267,27 @@ class RecMutAssocHandler(private val ctx: DbRecordsDaoCtx) {
             DbRecordsUtils.isChildAssocAttribute(it.attribute)
         }
 
-        val addedChildren = mutableSetOf<Long>()
-        val removedChildren = mutableSetOf<Long>()
+        val childrenChanges = HashMap<String, AddedRemovedAssocs>()
 
         for (att in childAssociations) {
-            val before = anyToSetOfLong(recBeforeSave.attributes[att.attribute.id])
-            val after = anyToSetOfLong(recAfterSave.attributes[att.attribute.id])
-            addedChildren.addAll(after.subtract(before))
-            removedChildren.addAll(before.subtract(after))
+            val attributeId = att.attribute.id
+            val before = anyToSetOfLong(recBeforeSave.attributes[attributeId])
+            val after = anyToSetOfLong(recAfterSave.attributes[attributeId])
+
+            val changes = AddedRemovedAssocs(
+                after.subtract(before),
+                before.subtract(after)
+            )
+            if (changes.isNotEmpty()) {
+                childrenChanges[attributeId] = changes
+            }
         }
 
         val changedChildren = mutableSetOf<Long>()
-        changedChildren.addAll(addedChildren)
-        changedChildren.addAll(removedChildren)
+        childrenChanges.values.forEach {
+            changedChildren.addAll(it.added)
+            changedChildren.addAll(it.removed)
+        }
 
         if (changedChildren.isEmpty()) {
             return
@@ -212,30 +295,38 @@ class RecMutAssocHandler(private val ctx: DbRecordsDaoCtx) {
 
         log.debug {
             val recRef = RecordRef.create(ctx.sourceId, recAfterSave.extId)
-            "Children of $recRef was changed. Added: $addedChildren Removed: $removedChildren"
+            "Children of $recRef was changed. " + childrenChanges.entries.joinToString {
+                it.key + ": added: " + it.value.added + " removed: " + it.value.removed
+            }
         }
 
-        val parentRefId = recAfterSave.refId
+        val parentRef = ctx.recordRefService.getRecordRefById(recAfterSave.refId)
 
         val childRefsById = ctx.recordRefService.getRecordRefsByIdsMap(changedChildren)
-        val addOrRemoveParentRef = { children: Set<Long>, add: Boolean ->
-            for (removedId in children) {
-                val childRef = childRefsById[removedId]
-                    ?: error("Child ref doesn't found by id. Refs: $childRefsById id: $removedId")
+        val addOrRemoveParentRef = { attId: String, children: Set<Long>, add: Boolean ->
+            for (childId in children) {
+                val childRef = childRefsById[childId]
+                    ?: error("Child ref doesn't found by id. Refs: $childRefsById id: $childId")
 
                 if (RecordRef.isNotEmpty(childRef)) {
                     val childAtts = RecordAtts(childRef)
                     if (add) {
-                        childAtts.setAtt(RecordConstants.ATT_PARENT, parentRefId)
+                        childAtts.setAtt(RecordConstants.ATT_PARENT, parentRef)
+                        childAtts.setAtt(RecordConstants.ATT_PARENT_ATT, attId)
                     } else {
                         childAtts.setAtt(RecordConstants.ATT_PARENT, null)
+                        childAtts.setAtt(RecordConstants.ATT_PARENT_ATT, null)
                     }
+                    childAtts.setAtt(MUTATION_FROM_PARENT_FLAG, true)
                     ctx.recordsService.mutate(childAtts)
                 }
             }
         }
-        addOrRemoveParentRef.invoke(removedChildren, false)
-        addOrRemoveParentRef.invoke(addedChildren, true)
+
+        childrenChanges.forEach {
+            addOrRemoveParentRef.invoke(it.key, it.value.removed, false)
+            addOrRemoveParentRef.invoke(it.key, it.value.added, true)
+        }
     }
 
     private fun anyToSetOfLong(value: Any?): Set<Long> {
@@ -253,5 +344,14 @@ class RecMutAssocHandler(private val ctx: DbRecordsDaoCtx) {
             return setOf(value)
         }
         return emptySet()
+    }
+
+    private data class AddedRemovedAssocs(
+        val added: Set<Long>,
+        val removed: Set<Long>
+    ) {
+        fun isNotEmpty(): Boolean {
+            return added.isNotEmpty() || removed.isNotEmpty()
+        }
     }
 }
