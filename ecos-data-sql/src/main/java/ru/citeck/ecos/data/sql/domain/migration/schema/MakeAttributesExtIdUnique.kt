61c451b095725948bdbd547c989c196d19528725
Pavel Simonov <pavel.simonov@citeck.ru>
2026-03-19 17:44:35 +0700

    COREDEV-3568 - Refactoring. Release 1.65.0

--- ecos-data-sql/src/main/java/ru/citeck/ecos/data/sql/domain/migration/schema/MakeAttributesExtIdUnique.kt
+++ ecos-data-sql/src/main/java/ru/citeck/ecos/data/sql/domain/migration/schema/MakeAttributesExtIdUnique.kt
@@ -1,10 +1,15 @@
 package ru.citeck.ecos.data.sql.domain.migration.schema
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import ru.citeck.ecos.data.sql.context.DbSchemaContext
 import ru.citeck.ecos.data.sql.records.assocs.DbAssocEntity
 import ru.citeck.ecos.data.sql.records.attnames.DbEcosAttributeEntity
 
 class MakeAttributesExtIdUnique : DbSchemaMigration {
+
+    companion object {
+        private val log = KotlinLogging.logger {}
+    }
 
     override fun run(context: DbSchemaContext) {
 
@@ -12,6 +17,8 @@ class MakeAttributesExtIdUnique : DbSchemaMigration {
         if (!context.isTableExists(tableRef)) {
             return
         }
+
+        log.info { "Migration started for ${tableRef.fullName}" }
 
         val dataSource = context.dataSourceCtx.dataSource
         val extId = DbEcosAttributeEntity.EXT_ID
@@ -23,7 +30,7 @@ class MakeAttributesExtIdUnique : DbSchemaMigration {
             if (!context.isTableExists(assocRef)) {
                 continue
             }
-            dataSource.update(
+            val remapped = dataSource.update(
                 "UPDATE ${assocRef.fullName} assoc SET \"$attCol\" = min_ids.min_id" +
                     " FROM ${tableRef.fullName} dup" +
                     " JOIN (SELECT \"$extId\", MIN(id) AS min_id FROM ${tableRef.fullName}" +
@@ -32,20 +39,24 @@ class MakeAttributesExtIdUnique : DbSchemaMigration {
                     " WHERE assoc.\"$attCol\" = dup.id AND dup.id > min_ids.min_id",
                 emptyList()
             )
+            log.info { "Remapped ${remapped.size} assoc rows in ${assocRef.fullName}" }
         }
 
         // Remove duplicates keeping the row with the smallest id
-        dataSource.update(
+        val deleted = dataSource.update(
             "DELETE FROM ${tableRef.fullName} a USING ${tableRef.fullName} b" +
                 " WHERE a.\"$extId\" = b.\"$extId\" AND a.id > b.id",
             emptyList()
         )
+        log.info { "Deleted ${deleted.size} duplicate rows from ${tableRef.fullName}" }
 
         // Create unique index (old non-unique index is redundant but harmless)
         dataSource.updateSchema(
             "CREATE UNIQUE INDEX IF NOT EXISTS \"${tableRef.table}_${extId}_unique\"" +
                 " ON ${tableRef.fullName} (\"$extId\")"
         )
+
+        log.info { "Migration completed for ${tableRef.fullName}" }
     }
 
     override fun getAppliedVersions(): Int {
